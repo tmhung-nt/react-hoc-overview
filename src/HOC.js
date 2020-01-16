9548d00d4ce588ae5ac35bdcfd3b1e7de52f435e
Hung Tran Manh <tranmanhhung@tma.com.vn>
2020-01-16 09:13:43 +0700

    simple HOC example

---
+++ src/HOC.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default function Hoc(HocComponent){
+    return class extends Component{
+        render(){
+            return (
+                <div>
+                    <HocComponent></HocComponent>
+                </div>
+
+            );
+        }
+    }
+}
