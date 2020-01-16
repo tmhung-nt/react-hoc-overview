fdc4c4bc54914518da72b03e5c8d8698e658d14f
Hung Tran Manh <tranmanhhung@tma.com.vn>
2020-01-16 09:19:12 +0700

    Add StockList and UserList which have duplicated code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import Hoc from './HOC';
+import StockList from "./StockList";
+import UserList from "./UserList";
 
 class App extends Component {
 
   render() {
     return (
         <div>
-          Higher-Order Component Tutorial
+          <StockList />
+          <hr/>
+          <UserList />
         </div>
     )
   }
 }
-App = Hoc(App);
+
 export default App;
