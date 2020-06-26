60ab9e52618ab10bb02020c236c3d1e07868d759
Hung Tran Manh <tranmanhhung@tma.com.vn>
2020-06-26 10:48:07 +0700

    add h1 heading

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ class App extends Component {
     render() {
         return (
             <div>
+                <h1>Azure DevOps</h1>
                 <Stocks/>
                 <hr/>
                 <Users/>
